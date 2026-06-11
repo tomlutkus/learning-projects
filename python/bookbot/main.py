8a30d554c79f014afc31455456bd09af40275d4d
Thomas Lutkus <tomlutkus@users.noreply.github.com>
2026-06-11 17:45:28 +0100

    bookbot/main.py refactored and split to stats.py

--- python/bookbot/main.py
+++ python/bookbot/main.py
@@ -1,11 +1,16 @@
+from stats import word_count
+
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     
     return file_contents
 
-def main(book_path):
-    book = get_book_text(book_path)
-    print(book)
+# def main(target_path):
+#     return get_book_text(target_path)
+
+def main(target_path):
+    book = get_book_text(target_path)
+    return word_count(book)
 
 main("books/frankenstein.txt")
---
+++ python/bookbot/stats.py
@@ -0,0 +1,4 @@
+def word_count(text_input):
+    words_from_text = text_input.split()
+    num_words = len(words_from_text)
+    print(f"Found {num_words} total words")
