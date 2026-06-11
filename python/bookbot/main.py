447e7f26981a447671c381614d7f6baf4117e330
Thomas Lutkus <tomlutkus@users.noreply.github.com>
2026-06-11 17:57:16 +0100

    bookbot/stats.py added get_char_count() and reworked get_word_count()

--- python/bookbot/main.py
+++ python/bookbot/main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import get_word_count, get_char_count
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -11,6 +11,13 @@ def get_book_text(file_path):
 
 def main(target_path):
     book = get_book_text(target_path)
-    return word_count(book)
+    num_words = get_word_count(book)
+    chars_list = get_char_count(book)
+
+    print(f"Found {num_words} total words")
+
+    for char in chars_list:
+        print(f"'{char}': {chars_list[char]}")
 
 main("books/frankenstein.txt")
+
--- python/bookbot/stats.py
+++ python/bookbot/stats.py
@@ -1,4 +1,17 @@
-def word_count(text_input):
+def get_word_count(text_input):
     words_from_text = text_input.split()
     num_words = len(words_from_text)
-    print(f"Found {num_words} total words")
+
+    return num_words
+
+
+def get_char_count(text_input):
+    char_count = {}
+
+    for char in text_input.lower():
+        if char not in char_count:
+            char_count[char] = 1
+        else:
+            char_count[char] += 1
+
+    return char_count
