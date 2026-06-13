ed49abe94cce758a02e7439f6ad1327fe33354f6
Thomas Lutkus <tomlutkus@users.noreply.github.com>
2026-06-13 15:51:34 +0100

    Added functionality to clear the screen and wrapped main code in main()

--- python/100-days/day010-calculator/main.py
+++ python/100-days/day010-calculator/main.py
@@ -11,30 +11,35 @@ def calculator(n1, n2, op):
     if op == "/":
         return n1 / n2
 
-new_calculation = True
-result = None
+def main():
+    new_calculation = True
+    result = None
 
-while True:
-    if new_calculation:
-        sys.write('\033[2J\033[H')
-        sys.flush()
-        print(logo)
-        first_number = float(input("What's the first number? "))
-    else:
-        first_number = result
-    
-    print("+\n-\n*\n/")
-    operation = input("Pick an operation: ")
-    second_number = float(input("What's the second number? "))
+    while True:
+        if new_calculation:
+            sys.stdout.write('\033[2J\033[H')
+            sys.stdout.flush()
+            print(logo)
+            first_number = float(input("What's the first number? "))
+        else:
+            first_number = result
+        
+        print("+\n-\n*\n/")
+        operation = input("Pick an operation: ")
+        second_number = float(input("What's the second number? "))
 
-    result = calculator(first_number, second_number, operation)
-    print(f"{first_number} {operation} {second_number} = {result}")
+        result = calculator(first_number, second_number, operation)
+        print(f"{first_number} {operation} {second_number} = {result}")
 
-    use_result = input(
-        f"Type 'y' to continue calculating with {result}, "
-        "or type 'n' to start a new calculation: "
-    )
-    if use_result == "y":
-        new_calculation = False
-    else:
-        new_calculation = True
+        use_result = input(
+            f"Type 'y' to continue calculating with {result}, "
+            "or type 'n' to start a new calculation: "
+        )
+        if use_result == "y":
+            new_calculation = False
+        else:
+            new_calculation = True
+
+
+if __name__ == "__main__":
+    main()
