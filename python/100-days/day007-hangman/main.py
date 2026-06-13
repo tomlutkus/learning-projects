905223550379bed3eb0871c5f10c880163334ac3
Thomas Lutkus <tomlutkus@users.noreply.github.com>
2026-06-13 15:42:36 +0100

    Moved the code from my old folder into the repo, renamed to main.py

---
+++ python/100-days/day007-hangman/main.py
@@ -0,0 +1,54 @@
+import random
+from art import stages, logo
+from words import word_list
+
+lives = 6
+
+print(logo)
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+    print(f"****************************{lives}/6 LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+    if guess in correct_letters:
+        print(f"You have already guessed the letter '{guess}'!")
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        print(f"The letter '{guess}' is not in the word! You lose a life!")
+        lives -= 1
+
+        if lives == 0:
+            game_over = True
+
+            # TODO 7: - Update the print statement below to give the user the correct word they were trying to guess.
+            print(f"***********************YOU LOSE**********************\n"
+                  f"The word was '{chosen_word}'.")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+    print(stages[lives])
