c4b4ad613e97656fb0f791e81d44e77a6786f278
Thomas Lutkus <tomlutkus@users.noreply.github.com>
2026-06-22 18:00:58 +0100

    Added tasks folder and added prime_check.py

---
+++ python/100-days/day012-number-guessing-game/tasks/prime_check.py
@@ -0,0 +1,31 @@
+import sys
+
+def check_if_prime(number):
+    if number < 2:
+        return False
+    if number == 2:
+        return True
+
+    for divisor in range(2, number):
+        if number % divisor == 0:
+            return False
+    
+    return True
+
+def main(number_input):
+    try:
+        number = int(number_input)
+    except ValueError:
+        raise ValueError("ERROR: Your input must be a positive integer.")
+    is_prime = check_if_prime(number)
+    if is_prime:
+        print(f"The number {number} is a prime number.")
+    else:
+        print(f"The number {number} is not a prime number.")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 prime_check.py <NUMBER>")
+        sys.exit(1)
+    main(sys.argv[1])
