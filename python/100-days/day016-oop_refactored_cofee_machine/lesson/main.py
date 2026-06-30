f4384672616d711b02889032a171daac8a183546
Thomas Lutkus <tomlutkus@users.noreply.github.com>
2026-06-30 12:18:48 +0100

    Lesson using PrettyTable class, commented previous lesson Turtle code

--- python/100-days/day016-oop_refactored_cofee_machine/lesson/main.py
+++ python/100-days/day016-oop_refactored_cofee_machine/lesson/main.py
@@ -1,11 +1,27 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("red", "green")
-timmy.forward(100)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red", "green")
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+my_table = PrettyTable()
+my_table.field_names = ["Pokemon Name", "Type"]
+my_table.add_rows(
+    [
+    ["Pikachu", "Electric"],
+    ["Squirtle", "Water"],
+    ["Charmander", "Fire"],
+    ]
+    )
+
+
+
+print(my_table)
