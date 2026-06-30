d3bb33d1896dbf187ceb057aae71e9211699329f
Thomas Lutkus <tomlutkus@users.noreply.github.com>
2026-06-30 12:26:29 +0100

    final adjustments to lesson's code

--- python/100-days/day016-oop_refactored_cofee_machine/lesson/main.py
+++ python/100-days/day016-oop_refactored_cofee_machine/lesson/main.py
@@ -16,11 +16,12 @@ my_table = PrettyTable()
 my_table.field_names = ["Pokemon Name", "Type"]
 my_table.add_rows(
     [
-    ["Pikachu", "Electric"],
-    ["Squirtle", "Water"],
-    ["Charmander", "Fire"],
+        ["Pikachu", "Electric"],
+        ["Squirtle", "Water"],
+        ["Charmander", "Fire"],
     ]
-    )
+)
+my_table.align = "l"
 
 
 
