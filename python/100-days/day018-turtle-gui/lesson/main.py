1761e07677c01d09f41ed4495e773d0b8505daa4
Thomas Lutkus <tomlutkus@users.noreply.github.com>
2026-07-07 19:13:03 +0100

    did some changes to the code

--- python/100-days/day018-turtle-gui/lesson/main.py
+++ python/100-days/day018-turtle-gui/lesson/main.py
@@ -1,14 +1,19 @@
-# from turtle import Turtle, Screen
 import turtle as t
+import tkinter
+
+screen = t.Screen()
+screen.setup(width=600, height=600)
 
 tim = t.Turtle()
 tim.shape("turtle")
 tim.color("black", "red")
-for _ in range(4):
-    tim.forward(100)
-    tim.right(90)
+tim.pensize(5)
 
+for _ in range(10):
+    tim.forward(20)
+    tim.pu()
+    tim.forward(20)
+    tim.pd()
 
-my_screen = t.Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+print(screen.canvheight)
+screen.exitonclick()
