e07f28ade3e1fe6a77e4e194b31c015f5cca4fe6
Thomas Lutkus <tomlutkus@users.noreply.github.com>
2026-07-08 21:41:27 +0100

    Added the scaffolding for the game's loop and screen drawing

--- python/asteroids/main.py
+++ python/asteroids/main.py
@@ -1,11 +1,22 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill("black")
+
+    while True:
+        log_state()
+
+        for even in pygame.event.get():
+            pass
+    pygame.display.flip()
 
 
 if __name__ == "__main__":
