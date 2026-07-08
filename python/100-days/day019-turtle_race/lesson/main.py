2dade64f4146bc089b953204374107abeee8b712
Thomas Lutkus <tomlutkus@users.noreply.github.com>
2026-07-08 15:14:11 +0100

    Created lesson scaffolding with the main.py file

---
+++ python/100-days/day019-turtle_race/lesson/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+import turtle
+
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+
+tim = Turtle()
+
+print(screen.canvheight)
+screen.exitonclick()
