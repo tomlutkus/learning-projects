056f732b179f99004e9d893249fc1fa073650173
Thomas Lutkus <tomlutkus@users.noreply.github.com>
2026-07-09 14:03:40 +0100

    Put the player object into the drawable and updatable pygame groups and changed the loop to use them instead of calling on the instance directly

--- python/asteroids/main.py
+++ python/asteroids/main.py
@@ -13,6 +13,10 @@ def main() -> None:
     clock = pygame.time.Clock()
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(x, y, PLAYER_RADIUS)
 
     while True:
@@ -22,9 +26,10 @@ def main() -> None:
                 return
 
         dt = clock.tick(60) / 1000
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
 
 
