03377bf1539ba9567ec3eeb1c1179a8bb01ddba7
Thomas Lutkus <tomlutkus@users.noreply.github.com>
2026-07-09 16:12:15 +0100

    Added asteroid and shot collision detection to the loop, to get the shots to kill asteroids

--- python/asteroids/main.py
+++ python/asteroids/main.py
@@ -43,6 +43,11 @@ def main() -> None:
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
         screen.fill("black")
         for item in drawable:
             item.draw(screen)
